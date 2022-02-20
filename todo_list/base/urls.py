aa5e5437adf6404420461781c5846dd6ddc80add
odhran <odhran.omahony@students.ittralee.ie>
2022-02-20 20:46:21 +0000

    display view task on page / correct typo

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -3,6 +3,6 @@ from .views import TaskDetail, TaskList
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
  
